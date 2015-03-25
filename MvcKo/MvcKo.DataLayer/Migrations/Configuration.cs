f4a4a82bf1eb76085851e47931ab3167945b0982
sebastianbadea <sebastian.badea@gmail.com>
2015-03-25 11:26:11 +0200

    set seeding to database

--- MvcKo/MvcKo.DataLayer/Migrations/Configuration.cs
+++ MvcKo/MvcKo.DataLayer/Migrations/Configuration.cs
@@ -3,6 +3,7 @@ using System.Data.Entity;
 using System.Data.Entity.Migrations;
 using System.Linq;
 using MvcKo.DataLayer;
+using MvcKo.Model;
 
 namespace MvcKo.DataLayer.Migrations
 {
@@ -16,19 +17,14 @@ namespace MvcKo.DataLayer.Migrations
 
         protected override void Seed(SalesContext context)
         {
-            
-            //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            context.SalesOrders.AddOrUpdate
+            (
+                so => so.CustomerName,
+                new SalesOrder { CustomerName = "Mike", PoNumber = "1" },
+                new SalesOrder { CustomerName = "Adam" },
+                new SalesOrder { CustomerName = "John", PoNumber = "2" }
+            );
         }
 
         
